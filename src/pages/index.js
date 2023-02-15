bb88beb7e2e138d28eddd093d80d946a6a9a32f2
Douglas Bett <bettdalpha@gmail.com>
2023-02-15 07:06:46 +0300

    Add first blog.

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,13 +22,6 @@ function HomepageHeader() {
           </Link>
         </div>
         <br></br>
-        {/* <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
-        </div> */}
       </div>
     </header>
   );
