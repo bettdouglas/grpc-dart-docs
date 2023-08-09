3e75079cf88859f0023b23913882c276fe60b417
Douglas Bett <bettdalpha@gmail.com>
2023-08-09 17:52:44 +0300

    Update site title & description

--- src/pages/index.js
+++ src/pages/index.js
@@ -31,8 +31,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`${siteConfig.title}`}
+      description="Learn how to build API's using gRPC & Dart">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
